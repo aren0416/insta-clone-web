7e0047371dc523e129b221d6dc4d9326e8e176d9
aren0416 <aren0416@gmail.com>
2022-05-21 19:31:50 +0900

    8.4 router setup part two

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./screens/Home";
+import { Login } from "./screens/Login";
+import { NotFound } from "./screens/NotFound";
 
 function App() {
+  const isLoggedIn = true;
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
   );
