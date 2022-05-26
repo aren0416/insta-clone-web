26d0f38538e93733ec03af696e4c779ee8ec0f87
aren0416 <aren0416@gmail.com>
2022-05-26 22:12:08 +0900

    10.8 React Hook Form is Awesome

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,6 +9,7 @@ import { AuthLayout } from "../components/auth/AuthLayout";
 import { BottomBox } from "../components/auth/BottomBox";
 import { Button } from "../components/auth/Button";
 import { FormBox } from "../components/auth/FormBox";
+import { FormError } from "../components/auth/FormError";
 import { Input } from "../components/auth/Input";
 import { Separator } from "../components/auth/Separator";
 import { PageTitle } from "../components/PageTitle";
@@ -23,14 +24,19 @@ const FacebookLogin = styled.div`
 `;
 
 export const Login = () => {
-  const { register, watch, handleSubmit } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
   const onSubmitValid = (data) => {
     console.log(data);
   };
 
-  const onSubmitInvalid = (data) => {
-    console.log(data);
-  };
+  console.log(errors);
 
   return (
     <AuthLayout>
@@ -39,25 +45,34 @@ export const Login = () => {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
           <Input
             {...register("username", {
               required: "아이디는 필수에요",
               minLength: {
-                value: 3,
+                value: 5,
+                message: "아이디는 5글자보다 길어야해요",
               },
             })}
             type="text"
             placeholder="Username"
+            hasError={Boolean(errors?.username?.message)}
           />
+          <FormError message={errors?.username?.message} />
           <Input
             {...register("password", {
               required: "비밀번호는 필수에요",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 8글자보다 길어야해요",
+              },
             })}
             type="password"
             placeholder="Password"
+            hasError={Boolean(errors?.password?.message)}
           />
-          <Button type="submit" value="Log in" />
+          <FormError message={errors?.password?.message} />
+          <Button type="submit" value="Log in" disabled={!isValid} />
         </form>
         <Separator />
         <FacebookLogin>
