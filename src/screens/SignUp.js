de736d1700b8da024053f6ea1b411f11071b6b56
aren0416 <aren0416@gmail.com>
2022-05-27 22:41:22 +0900

    10.12 Create Account

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,6 +1,8 @@
+import { gql, useMutation } from "@apollo/client";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { AuthLayout } from "../components/auth/AuthLayout";
 import { BottomBox } from "../components/auth/BottomBox";
@@ -10,6 +12,7 @@ import { Input } from "../components/auth/Input";
 import { Separator } from "../components/auth/Separator";
 import { PageTitle } from "../components/PageTitle";
 import { FatLInk } from "../components/shared";
+import { FormError } from "../components/auth/FormError";
 import { routes } from "../routes";
 
 const HeaderContainer = styled.div`
@@ -24,7 +27,63 @@ const Subtitle = styled(FatLInk)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 export const SignUp = () => {
+  const navigate = useNavigate();
+
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    navigate(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    // const { firstName, lastName, email, username, password } = data;
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
   return (
     <AuthLayout>
       <PageTitle title="Sign up" />
@@ -36,12 +95,49 @@ export const SignUp = () => {
           </Subtitle>
         </HeaderContainer>
         <Separator />
-        <form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", {
+              required: "이름은 필수에요",
+            })}
+            type="text"
+            placeholder="First Name"
+          />
+          <FormError message={errors?.firstName?.message} />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("email", {
+              required: "이메일은 필수에요",
+            })}
+            type="text"
+            placeholder="Email"
+          />
+          <FormError message={errors?.email?.message} />
+          <Input
+            {...register("username", {
+              required: "아이디는 필수에요",
+            })}
+            type="text"
+            placeholder="Username"
+          />
+          <FormError message={errors?.username?.message} />
+          <Input
+            {...register("password", {
+              required: "비밀번호 필수에요",
+            })}
+            type="password"
+            placeholder="Password"
+          />
+          <FormError message={errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading.." : "Sign up"}
+            disabled={!isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta="Have an account?" link={routes.home} linkText="Log in" />
